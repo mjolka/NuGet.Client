164b6732f94b270932f307060bf716312668cea9
Fernando Aguilar <feaguila@microsoft.com>
2022-02-14 21:25:56 -0600

    First working draft
    - Wired to PM UI
    - Sorting View Models in PM UI
    - Updated comments

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/PackageFeeds/InstalledAndTransitivePackageFeed.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/PackageFeeds/InstalledAndTransitivePackageFeed.cs
@@ -12,31 +12,36 @@ using NuGet.VisualStudio.Internal.Contracts;
 
 namespace NuGet.PackageManagement.VisualStudio
 {
-    /// <summary>
-    /// Represents a package feed enumerating installed packages.
-    /// </summary>
-    public sealed class InstalledPackageFeed : PlainPackageFeedBase
+    public sealed class InstalledAndTransitivePackageFeed : PlainPackageFeedBase
     {
         private readonly IEnumerable<PackageCollectionItem> _installedPackages;
+        private readonly IEnumerable<PackageCollectionItem> _transitivePackages;
         private readonly IPackageMetadataProvider _metadataProvider;
 
-        public InstalledPackageFeed(
-            IEnumerable<PackageCollectionItem> installedPackages,
-            IPackageMetadataProvider metadataProvider)
+        public InstalledAndTransitivePackageFeed(IEnumerable<PackageCollectionItem> installedPackages, IEnumerable<PackageCollectionItem> transitivePackages, IPackageMetadataProvider metadataProvider)
         {
             if (installedPackages == null)
             {
                 throw new ArgumentNullException(nameof(installedPackages));
             }
-            _installedPackages = installedPackages;
+
+            if (transitivePackages == null)
+            {
+                throw new ArgumentNullException(nameof(transitivePackages));
+            }
 
             if (metadataProvider == null)
             {
                 throw new ArgumentNullException(nameof(metadataProvider));
             }
+
+            _installedPackages = installedPackages;
+            _transitivePackages = transitivePackages;
             _metadataProvider = metadataProvider;
         }
 
+
+        /// <inheritdoc cref="IPackageFeed.ContinueSearchAsync(ContinuationToken, CancellationToken)" />
         public override async Task<SearchResult<IPackageSearchMetadata>> ContinueSearchAsync(ContinuationToken continuationToken, CancellationToken cancellationToken)
         {
             var searchToken = continuationToken as FeedSearchContinuationToken;
@@ -45,15 +50,20 @@ namespace NuGet.PackageManagement.VisualStudio
                 throw new InvalidOperationException(Strings.Exception_InvalidContinuationToken);
             }
 
-            PackageIdentity[] packages = _installedPackages
-                .GetLatest()
+            // Remove foreign transitive packages
+            IEnumerable<PackageCollectionItem> pkgsWithOrigins = _transitivePackages
+                .Where(t => t.PackageReferences.Any(x => x is ITransitivePackageReferenceContextInfo y && y.TransitiveOrigins.Any()));
+
+            IEnumerable<PackageCollectionItem> pkgs = _installedPackages.Concat(pkgsWithOrigins);
+
+            PackageCollectionItem[] allPkgs = pkgs
                 .Where(p => p.Id.IndexOf(searchToken.SearchString, StringComparison.OrdinalIgnoreCase) != -1)
                 .OrderBy(p => p.Id)
                 .Skip(searchToken.StartIndex)
                 .ToArray();
 
             IEnumerable<IPackageSearchMetadata> items = await TaskCombinators.ThrottledAsync(
-                packages,
+                allPkgs,
                 (p, t) => GetPackageMetadataAsync(p, searchToken.SearchFilter.IncludePrerelease, t),
                 cancellationToken);
 
@@ -61,9 +71,7 @@ namespace NuGet.PackageManagement.VisualStudio
             //  however the asynchronous execution has randomly reordered the set. So we need to resort. 
             SearchResult<IPackageSearchMetadata> result = SearchResult.FromItems(items.OrderBy(p => p.Identity.Id).ToArray());
 
-            var loadingStatus = packages.Length == 0
-                ? LoadingStatus.NoItemsFound
-                : LoadingStatus.NoMoreItems;
+            var loadingStatus = allPkgs.Length == 0 ? LoadingStatus.NoItemsFound : LoadingStatus.NoMoreItems;
             result.SourceSearchStatus = new Dictionary<string, LoadingStatus>
             {
                 { "Installed", loadingStatus }
@@ -72,7 +80,7 @@ namespace NuGet.PackageManagement.VisualStudio
             return result;
         }
 
-        public async Task<IPackageSearchMetadata> GetPackageMetadataAsync(PackageIdentity identity, bool includePrerelease, CancellationToken cancellationToken)
+        internal async Task<IPackageSearchMetadata> GetPackageMetadataAsync(PackageCollectionItem identity, bool includePrerelease, CancellationToken cancellationToken)
         {
             // first we try and load the metadata from a local package
             var packageMetadata = await _metadataProvider.GetLocalPackageMetadataAsync(identity, includePrerelease, cancellationToken);
@@ -81,6 +89,26 @@ namespace NuGet.PackageManagement.VisualStudio
                 // and failing that we go to the network
                 packageMetadata = await _metadataProvider.GetPackageMetadataAsync(identity, includePrerelease, cancellationToken);
             }
+
+            IEnumerable<IPackageReferenceContextInfo> transPRs = identity.PackageReferences.Where(t => t is ITransitivePackageReferenceContextInfo);
+
+            IPackageReferenceContextInfo transPR = transPRs.OrderByDescending(x => x.Identity.Version).FirstOrDefault();
+
+            var transPRObject = transPR as TransitivePackageReferenceContextInfo;
+
+            IReadOnlyCollection<PackageIdentity> res = Array.Empty<PackageIdentity>();
+            if (transPRObject != null)
+            {
+                res = transPRObject.TransitiveOrigins?.Select(to => to.Identity)?.ToArray() ?? Array.Empty<PackageIdentity>();
+            }
+
+            if (res.Any())
+            {
+                var ts = new TransitivePackageSearchMetadata(packageMetadata, res);
+
+                return ts;
+            }
+
             return packageMetadata;
         }
     }
