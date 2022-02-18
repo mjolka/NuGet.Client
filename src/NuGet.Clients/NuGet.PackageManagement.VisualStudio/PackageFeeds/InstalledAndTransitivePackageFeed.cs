3eaba616cac26ff07c089988500e33c86bf67767
Fernando Aguilar <feaguila@microsoft.com>
2022-02-18 01:09:57 -0600

    Feedback: Unseal and derive InstalledPackageFeed

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/PackageFeeds/InstalledAndTransitivePackageFeed.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/PackageFeeds/InstalledAndTransitivePackageFeed.cs
@@ -12,34 +12,15 @@ using NuGet.VisualStudio.Internal.Contracts;
 
 namespace NuGet.PackageManagement.VisualStudio
 {
-    public sealed class InstalledAndTransitivePackageFeed : PlainPackageFeedBase
+    public sealed class InstalledAndTransitivePackageFeed : InstalledPackageFeed
     {
-        private readonly IEnumerable<PackageCollectionItem> _installedPackages;
         private readonly IEnumerable<PackageCollectionItem> _transitivePackages;
-        private readonly IPackageMetadataProvider _metadataProvider;
 
         public InstalledAndTransitivePackageFeed(IEnumerable<PackageCollectionItem> installedPackages, IEnumerable<PackageCollectionItem> transitivePackages, IPackageMetadataProvider metadataProvider)
+            : base(installedPackages, metadataProvider)
         {
-            if (installedPackages == null)
-            {
-                throw new ArgumentNullException(nameof(installedPackages));
-            }
-
-            if (transitivePackages == null)
-            {
-                throw new ArgumentNullException(nameof(transitivePackages));
-            }
-
-            if (metadataProvider == null)
-            {
-                throw new ArgumentNullException(nameof(metadataProvider));
-            }
-
-            _installedPackages = installedPackages;
-            _transitivePackages = transitivePackages;
-            _metadataProvider = metadataProvider;
+            _transitivePackages = transitivePackages ?? throw new ArgumentNullException(nameof(transitivePackages));
         }
-
 
         /// <inheritdoc cref="IPackageFeed.ContinueSearchAsync(ContinuationToken, CancellationToken)" />
         public override async Task<SearchResult<IPackageSearchMetadata>> ContinueSearchAsync(ContinuationToken continuationToken, CancellationToken cancellationToken)
@@ -50,66 +31,49 @@ namespace NuGet.PackageManagement.VisualStudio
                 throw new InvalidOperationException(Strings.Exception_InvalidContinuationToken);
             }
 
-            // Remove foreign transitive packages
+            // Remove transitive packages from project references
             IEnumerable<PackageCollectionItem> pkgsWithOrigins = _transitivePackages
                 .Where(t => t.PackageReferences.Any(x => x is ITransitivePackageReferenceContextInfo y && y.TransitiveOrigins.Any()));
 
             IEnumerable<PackageCollectionItem> pkgs = _installedPackages.Concat(pkgsWithOrigins);
 
-            PackageCollectionItem[] allPkgs = pkgs
-                .Where(p => p.Id.IndexOf(searchToken.SearchString, StringComparison.OrdinalIgnoreCase) != -1)
-                .OrderBy(p => p.Id)
-                .Skip(searchToken.StartIndex)
-                .ToArray();
+            PackageCollectionItem[] allPkgs = PerformLookup(pkgs, searchToken);
 
             IEnumerable<IPackageSearchMetadata> items = await TaskCombinators.ThrottledAsync(
                 allPkgs,
                 (p, t) => GetPackageMetadataAsync(p, searchToken.SearchFilter.IncludePrerelease, t),
                 cancellationToken);
 
-            //  The packages were originally sorted which is important because we Skip based on that sort
-            //  however the asynchronous execution has randomly reordered the set. So we need to resort. 
-            SearchResult<IPackageSearchMetadata> result = SearchResult.FromItems(items.OrderBy(p => p.Identity.Id).ToArray());
-
-            var loadingStatus = allPkgs.Length == 0 ? LoadingStatus.NoItemsFound : LoadingStatus.NoMoreItems;
-            result.SourceSearchStatus = new Dictionary<string, LoadingStatus>
-            {
-                { "Installed", loadingStatus }
-            };
-
-            return result;
+            return CreateResult(items);
         }
 
         internal async Task<IPackageSearchMetadata> GetPackageMetadataAsync(PackageCollectionItem identity, bool includePrerelease, CancellationToken cancellationToken)
         {
-            // first we try and load the metadata from a local package
-            var packageMetadata = await _metadataProvider.GetLocalPackageMetadataAsync(identity, includePrerelease, cancellationToken);
-            if (packageMetadata == null)
+            IEnumerable<ITransitivePackageReferenceContextInfo> transitivePRs = identity.PackageReferences.OfType<ITransitivePackageReferenceContextInfo>();
+
+            ITransitivePackageReferenceContextInfo transitivePR = transitivePRs.OrderByDescending(x => x.Identity.Version).FirstOrDefault();
+
+            if (transitivePR != null)
             {
-                // and failing that we go to the network
-                packageMetadata = await _metadataProvider.GetPackageMetadataAsync(identity, includePrerelease, cancellationToken);
+                IReadOnlyCollection<PackageIdentity> transitiveOrigins = transitivePR.TransitiveOrigins?.Select(to => to.Identity).ToArray() ?? Array.Empty<PackageIdentity>();
+
+                if (transitiveOrigins.Any())
+                {
+                    // Get only local metadata. We don't want Deprecation and Vulnerabilities Metadata on Transitive packages
+                    var packageMetadata = await _metadataProvider.GetLocalPackageMetadataAsync(identity, includePrerelease, cancellationToken);
+
+                    var ts = new TransitivePackageSearchMetadata(packageMetadata, transitiveOrigins);
+                    return ts;
+                }
+                else
+                {
+                    return await base.GetPackageMetadataAsync(identity, includePrerelease, cancellationToken);
+                }
             }
-
-            IEnumerable<IPackageReferenceContextInfo> transPRs = identity.PackageReferences.Where(t => t is ITransitivePackageReferenceContextInfo);
-
-            IPackageReferenceContextInfo transPR = transPRs.OrderByDescending(x => x.Identity.Version).FirstOrDefault();
-
-            var transPRObject = transPR as TransitivePackageReferenceContextInfo;
-
-            IReadOnlyCollection<PackageIdentity> res = Array.Empty<PackageIdentity>();
-            if (transPRObject != null)
+            else
             {
-                res = transPRObject.TransitiveOrigins?.Select(to => to.Identity)?.ToArray() ?? Array.Empty<PackageIdentity>();
+                return await base.GetPackageMetadataAsync(identity, includePrerelease, cancellationToken);
             }
-
-            if (res.Any())
-            {
-                var ts = new TransitivePackageSearchMetadata(packageMetadata, res);
-
-                return ts;
-            }
-
-            return packageMetadata;
         }
     }
 }
--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/PackageFeeds/InstalledPackageFeed.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/PackageFeeds/InstalledPackageFeed.cs
@@ -15,10 +15,10 @@ namespace NuGet.PackageManagement.VisualStudio
     /// <summary>
     /// Represents a package feed enumerating installed packages.
     /// </summary>
-    public sealed class InstalledPackageFeed : PlainPackageFeedBase
+    public class InstalledPackageFeed : PlainPackageFeedBase
     {
-        private readonly IEnumerable<PackageCollectionItem> _installedPackages;
-        private readonly IPackageMetadataProvider _metadataProvider;
+        internal readonly IEnumerable<PackageCollectionItem> _installedPackages;
+        internal readonly IPackageMetadataProvider _metadataProvider;
 
         public InstalledPackageFeed(
             IEnumerable<PackageCollectionItem> installedPackages,
@@ -45,25 +45,32 @@ namespace NuGet.PackageManagement.VisualStudio
                 throw new InvalidOperationException(Strings.Exception_InvalidContinuationToken);
             }
 
-            PackageIdentity[] packages = _installedPackages
-                .GetLatest()
-                .Where(p => p.Id.IndexOf(searchToken.SearchString, StringComparison.OrdinalIgnoreCase) != -1)
-                .OrderBy(p => p.Id)
-                .Skip(searchToken.StartIndex)
-                .ToArray();
+            PackageIdentity[] packages = PerformLookup(_installedPackages.GetLatest(), searchToken);
 
             IEnumerable<IPackageSearchMetadata> items = await TaskCombinators.ThrottledAsync(
                 packages,
                 (p, t) => GetPackageMetadataAsync(p, searchToken.SearchFilter.IncludePrerelease, t),
                 cancellationToken);
 
+            return CreateResult(items);
+        }
+
+        internal static T[] PerformLookup<T>(IEnumerable<T> items, FeedSearchContinuationToken token) where T : PackageIdentity
+        {
+            return items
+                .Where(p => p.Id.IndexOf(token.SearchString, StringComparison.OrdinalIgnoreCase) != -1)
+                .OrderBy(p => p.Id)
+                .Skip(token.StartIndex)
+                .ToArray();
+        }
+
+        internal SearchResult<IPackageSearchMetadata> CreateResult(IEnumerable<IPackageSearchMetadata> items)
+        {
             //  The packages were originally sorted which is important because we Skip based on that sort
             //  however the asynchronous execution has randomly reordered the set. So we need to resort. 
             SearchResult<IPackageSearchMetadata> result = SearchResult.FromItems(items.OrderBy(p => p.Identity.Id).ToArray());
 
-            var loadingStatus = packages.Length == 0
-                ? LoadingStatus.NoItemsFound
-                : LoadingStatus.NoMoreItems;
+            var loadingStatus = result.Any() ? LoadingStatus.NoMoreItems : LoadingStatus.NoItemsFound;
             result.SourceSearchStatus = new Dictionary<string, LoadingStatus>
             {
                 { "Installed", loadingStatus }
@@ -72,7 +79,7 @@ namespace NuGet.PackageManagement.VisualStudio
             return result;
         }
 
-        public async Task<IPackageSearchMetadata> GetPackageMetadataAsync(PackageIdentity identity, bool includePrerelease, CancellationToken cancellationToken)
+        internal async Task<IPackageSearchMetadata> GetPackageMetadataAsync(PackageIdentity identity, bool includePrerelease, CancellationToken cancellationToken)
         {
             // first we try and load the metadata from a local package
             var packageMetadata = await _metadataProvider.GetLocalPackageMetadataAsync(identity, includePrerelease, cancellationToken);
